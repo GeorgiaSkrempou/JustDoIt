ce288db27ca0e3122055d878bd80ac3b01b940f6
georgia <georgiaskrempou@gmail.com>
2022-04-18 19:10:31 +0300

    feat:added database models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class ToDoList(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Item(models.Model):
+    # django doesn't know the type of field of ToDoList because it's an object defined outside django, that's why we use the foreign key
+    todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+    complete = models.BooleanField()
+
+    def __str__(self):
+        return self.text
