f752b565ad831019e1a25061a131edd0d2997f29
georgia <georgiaskrempou@gmail.com>
2022-04-19 21:02:47 +0300

    fix:fixed index error

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,6 @@ from main import views
 urlpatterns = [
     path("<str:name>", views.index, name = "index"),
     path("", views.home, name = "home"),
+    path("create/", views.create, name = "create"),
+
 ]
