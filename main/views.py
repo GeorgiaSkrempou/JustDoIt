db2dd777a332ff8dc38469565ad64574d32168e6
georgia <georgiaskrempou@gmail.com>
2022-04-19 22:09:48 +0300

    feat: added custom forms for adding and checking item

--- main/views.py
+++ main/views.py
@@ -6,6 +6,25 @@ from .forms import CreateNewList
 
 def index(response, name):
     ls = ToDoList.objects.get(name=name)
+    if response.method == "POST":
+        print(response.POST)
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c" + str(item.name)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+
+                item.save()
+
+
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+            if len(txt) > 2:
+                ls.item_set.create(text=txt, complete = False)
+            else:
+                print("invalid")
+
     return render(response, "main/list.html", {"ls":ls})
     
 def home(response):
