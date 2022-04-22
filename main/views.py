59e5ff2053da5ac7b0cf511175bac69e81499522
georgia <georgiaskrempou@gmail.com>
2022-04-22 21:22:29 +0300

    chore: turned create form into a custom-made form

--- main/views.py
+++ main/views.py
@@ -14,7 +14,7 @@ def index(response, id):
 
     if ls in response.user.todolist.all():
         if response.method == "POST":
-            print(response.POST)
+            #print(response.POST)
             if response.POST.get("save"):
                 for item in ls.item_set.all():
                     if response.POST.get("c" + str(item.id)) == "clicked":
@@ -39,20 +39,39 @@ def index(response, id):
 def home(response):
     return render(response, "main/home.html", {})
 
+# @login_required
+# def create(response):
+#     if response.method == "POST":
+#         form = CreateNewList(response.POST)
+#         if form.is_valid():
+#             n = form.cleaned_data["name"]
+#             t = ToDoList(name=n)
+#             t.save()
+#             response.user.todolist.add(t)
+
+#         return HttpResponseRedirect(f"/{t.id}")
+#     else: 
+#         form = CreateNewList()
+#     return render(response, "main/create.html", {"form":form})   
+
 @login_required
 def create(response):
-    if response.method == "POST":
-        form = CreateNewList(response.POST)
-        if form.is_valid():
-            n = form.cleaned_data["name"]
-            t = ToDoList(name=n)
-            t.save()
-            response.user.todolist.add(t)
+    ls = ToDoList.objects.all()
 
-        return HttpResponseRedirect(f"/{t.id}")
-    else: 
-        form = CreateNewList()
-    return render(response, "main/create.html", {"form":form})   
+    if response.method == "POST":
+        if response.POST.get("newList"):
+            txt = response.POST.get("new")
+            if len(txt) > 2:
+                t=ToDoList(name=txt)
+                t.save()
+                response.user.todolist.add(t)
+                return HttpResponseRedirect(f"/{t.id}")
+            else:
+                print("invalid")
+            
+    return render(response, "main/create.html", {})
+
+
 
 
 @login_required
